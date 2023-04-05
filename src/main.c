5ee502ff2565310b5d6c454723a6dd34146b6ad0
Nikita Biryukov <chppppr@yandex.ru>
2023-04-05 23:31:37 +0700

    Add glib library, #8

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <glib.h>
 #include <stdio.h>
 
 #include <directory.h>
@@ -13,5 +14,15 @@ int main()
     traverse_list(&dir_list, print);
     list_clear(&dir_list);
 
+    GSList *list = NULL, *iterator = NULL;
+    list = g_slist_append(list, "hello");
+    list = g_slist_append(list, "world");
+    list = g_slist_append(list, "application");
+
+    for (iterator = list; iterator; iterator = iterator->next) {
+        printf("Current item is '%s'\n", (char*)iterator->data);
+    }
+
+    g_slist_free(list);
     return 0;
 }

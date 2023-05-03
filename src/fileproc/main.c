27eb0903ec2129de6fb9bbf5f2bcc4021083cb1a
Kulik Pavel <KashaPulik@users.noreply.github.com>
2023-05-04 01:31:14 +0700

    Fix header files in main, #20

--- src/fileproc/main.c
+++ src/fileproc/main.c
@@ -1,14 +1,10 @@
-#include <glib.h>
 #include <locale.h>
-#include <ncurses.h>
-#include <stdio.h>
 
 #include <fileproc/interface.h>
 #include <libfileproc/directory.h>
 #include <libfileproc/lexer.h>
 #include <libfileproc/rename.h>
 
-// driver
 int main()
 {
     setlocale(LC_ALL, "");
